7c59277c57dc96154a517866233cd31feeddb510
wilfried <153339362+wahaha-1@users.noreply.github.com>
2024-05-14 10:20:34 +0800

    Delete image recognition algorithm based on Graph Neural Networks directory

--- image recognition algorithm based on Graph Neural Networks/data/load_data.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch_geometric.datasets import MNISTSuperpixels
-from torch_geometric.data import DataLoader
-
-def load_data(batch_size=64, train=True):
-    dataset = MNISTSuperpixels(root='/tmp/MNISTSuperpixels', train=train)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=train)
-    return loader
